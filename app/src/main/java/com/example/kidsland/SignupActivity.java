ac2ebcff9f1144c03ed4752a79e3a02aa0b49de1
IkramErraghay <ikramerraghay2281@gmail.com>
2023-11-08 18:30:58 +0100

    adding authentification(sign-in sign-up)

--- app/src/main/java/com/example/kidsland/SignupActivity.java
+++ app/src/main/java/com/example/kidsland/SignupActivity.java
@@ -1,19 +1,35 @@
 package com.example.kidsland;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextFullName, editTextPassword, editTextConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Initialisez mAuth avec une instance de FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         // Listener for the sign-in text
         TextView signInText = findViewById(R.id.alreadyHaveAccountTextView);
@@ -26,18 +42,47 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextFullName = findViewById(R.id.editTextFullName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
         // Listener for the Register button
         Button registerButton = findViewById(R.id.registerbutton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Implement the registration logic here
-                // After registration, you can start the SignInActivity
-                // For now, we just navigate to the SignInActivity
-                Intent intent = new Intent(SignupActivity.this, SinginActivity.class);
-                startActivity(intent);
+                String email = editTextEmail.getText().toString();
+                String fullName = editTextFullName.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String confirmPassword = editTextConfirmPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(fullName) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                    Toast.makeText(SignupActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(SignupActivity.this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Use Firebase Auth to create a new user account
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Registration was successful
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(SignupActivity.this, "Registration success", Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(SignupActivity.this, SinginActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // Registration failed
+                                        Toast.makeText(SignupActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        Log.e("FirebaseError", task.getException().getMessage());
+                                    }
+                                }
+                            });
+                }
             }
         });
     }
-
 }
--- app/src/main/java/com/example/kidsland/SinginActivity.java
+++ app/src/main/java/com/example/kidsland/SinginActivity.java
@@ -1,28 +1,70 @@
 package com.example.kidsland;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SinginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singin);
 
-        // Configurer le bouton Sign In pour qu'il soit cliquable
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+
         Button signInButton = findViewById(R.id.signinbutton);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Créer un Intent pour démarrer CategorieActivity
-                Intent intent = new Intent(SinginActivity.this, CategorieActivity.class);
-                startActivity(intent);
+                // Get user input from the EditText fields
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(SinginActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SinginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign-in was successful
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(SinginActivity.this, "Sign-in success.", Toast.LENGTH_SHORT).show();
+
+
+                                        Intent intent = new Intent(SinginActivity.this, CategorieActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        // Sign-in failed
+                                        Toast.makeText(SinginActivity.this, "Sign-in failed.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
             }
         });
 
@@ -37,4 +79,6 @@ public class SinginActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
